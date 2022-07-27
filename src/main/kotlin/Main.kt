58dbe74c90bcead1cd62946bb8ecafd25af5590d
Jewel <zxdmjr@gmail.com>
2022-07-27 22:48:27 +0600

    feat: solve PalindromeNumber

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,8 @@
-import convert.StringToInt
+import palindrome.PalindromeNumber
 
 fun main(args: Array<String>) {
 
-//    print(ReverseInteger().reverse(10))
-//    println(StringToInt().myAtoi("+-12"))
-//    println(StringToInt().myAtoi("42"))
-//    println(StringToInt().myAtoi("-91283472332"))
-//    println(StringToInt().myAtoi("0000-432a323"))
-//    println(StringToInt().myAtoi("00002147483646-432a323"))
-//    println(StringToInt().myAtoi("   00000000000012345678"))
-//    println(StringToInt().myAtoi("42"))
-//    println(StringToInt().myAtoi("    -42"))
-//    println(StringToInt().myAtoi("-91283472332"))
-//    println(StringToInt().myAtoi("4193 with words"))
-//    println(StringToInt().myAtoi("--343"))
-//    println(StringToInt().myAtoi("0000-432a323"))
-//    println(StringToInt().myAtoi("00002147483646-432a323"))
-//    println(StringToInt().myAtoi("   00000000000012345678"))
-//    println(StringToInt().myAtoi("21474836460"))
-
+    println(PalindromeNumber().isPalindrome(121))
+    println(PalindromeNumber().isPalindrome(-121))
+    println(PalindromeNumber().isPalindrome(10))
 }
--- src/main/kotlin/palindrome/PalindromeNumber.kt
+++ src/main/kotlin/palindrome/PalindromeNumber.kt
@@ -3,13 +3,16 @@ package palindrome
 class PalindromeNumber {
 
     fun isPalindrome(x: Int): Boolean {
+        if(x < 0)
+            return false
+        if(x in 0..9) return true
         var num = x
         var rev = 0
         while(num != 0){
             rev = (rev * 10) + num % 10
             num /= 10
         }
-        return rev >= -2147483648 && rev <= 2147483647 && rev == x
+        return rev in 10..2147483647 && rev == x
     }
 
 }
