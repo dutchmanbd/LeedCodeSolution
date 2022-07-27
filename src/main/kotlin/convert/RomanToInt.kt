c51b4324f373c16d2c3d76401bdf8c7b08985335
Jewel <zxdmjr@gmail.com>
2022-07-27 23:28:40 +0600

    feat: solve RomanToInt without map

--- src/main/kotlin/convert/RomanToInt.kt
+++ src/main/kotlin/convert/RomanToInt.kt
@@ -1,21 +1,21 @@
 package convert
 
 class RomanToInt {
-    private val dic = mapOf(
-        "M" to 1000,
-        "CM" to 900,
-        "D" to 500,
-        "CD" to 400,
-        "C" to 100,
-        "XC" to 90,
-        "L" to 50,
-        "XL" to 40,
-        "X" to 10,
-        "IX" to 9,
-        "V" to 5,
-        "IV" to 4,
-        "I" to 1
-    )
+//    private val dic = mapOf(
+//        "M" to 1000,
+//        "CM" to 900,
+//        "D" to 500,
+//        "CD" to 400,
+//        "C" to 100,
+//        "XC" to 90,
+//        "L" to 50,
+//        "XL" to 40,
+//        "X" to 10,
+//        "IX" to 9,
+//        "V" to 5,
+//        "IV" to 4,
+//        "I" to 1
+//    )
 
     fun romanToInt(s: String): Int {
         var num = 0
@@ -41,14 +41,12 @@ class RomanToInt {
                 num += 4
                 index += 2
             } else {
-                val key = "${s[index]}"
-                num += dic[key]!!
+                num += s[index].toRoman()
                 index++
             }
         }
         if (index < length) {
-            val key = "${s[index]}"
-            num += dic[key]!!
+            num += s[index].toRoman()
         }
         return num
     }
