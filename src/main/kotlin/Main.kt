297c6f98802cc2947bc0f7a3e285b5570c57db8c
Jewel Rana <jewel@ticonsys.com>
2022-07-28 16:53:12 +0600

    feat: solve PalindromeLinkedList

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,24 @@
 import common.RansomNote
+import palindrome.ListNode
+import palindrome.PalindromeLinkedList
 
 
 fun main(args: Array<String>) {
 
-    println(RansomNote().canConstruct("a", "b"))
-    println(RansomNote().canConstruct("aa", "ab"))
-    println(RansomNote().canConstruct("aa", "aab"))
+    val l1 = ListNode(1)
+    val l2 = ListNode(2).apply {
+        next = l1
+    }
+    val l3 = ListNode(2).apply {
+        next = l2
+    }
+    val l4 = ListNode(4).apply {
+        next = l3
+    }
+    val head = ListNode(1).apply {
+        next = l4
+    }
+
+    println(PalindromeLinkedList().isPalindrome(head))
 
 }
---
+++ src/main/kotlin/palindrome/PalindromeLinkedList.kt
@@ -0,0 +1,30 @@
+package palindrome
+
+class PalindromeLinkedList {
+    fun isPalindrome(head: ListNode?): Boolean {
+        if (head == null) return false
+        val numbers = mutableListOf<Int>()
+        var currentHead = head
+        numbers.add(head.`val`)
+        while (currentHead?.next != null) {
+            numbers.add(currentHead.next?.`val` ?: 0)
+            currentHead = currentHead.next
+        }
+
+        var left = 0
+        var right = numbers.size - 1
+        while (left < right) {
+            if (numbers[left] != numbers[right]) {
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
+
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
